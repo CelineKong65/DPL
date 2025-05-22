db389b11a12bf4eb466fa6054e1d06998d25048d
tingxuan30 <xuanting.0330@gmail.com>
2025-05-23 00:17:51 +0800

    remove the unsed helper function raw_replace

--- GROUP_1.py
+++ GROUP_1.py
@@ -167,17 +167,6 @@ def raw_items(dictionary):
     for key in dictionary:
         items.append((key, dictionary[key]))
     return items
-def raw_replace(s, old, new):
-    result = ""
-    i = 0
-    while i < len(s):
-        if s[i:i+len(old)] == old:
-            result += new
-            i += len(old)
-        else:
-            result += s[i]
-            i += 1
-    return result
 def get_quoted_field(ss):
     field = ""
     ss = ss.strip()
